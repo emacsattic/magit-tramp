170cc549b045248355a6c9850627b1f9266c9882
Yann Hodique <yann.hodique@gmail.com>
2013-08-04 22:33:11 +0200

    works with directories now
    also make paths absolute:
    
    /git:HEAD@magit:/magit.el

--- magit-tramp.el
+++ magit-tramp.el
@@ -38,6 +38,9 @@
   (let ((v (tramp-dissect-file-name filename)))
     (string= (tramp-file-name-method v) magit-tramp-method)))
 
+(defsubst magit-tramp-filerev (user localname)
+  (format "%s:%s" user (substring localname 1)))
+
 (defvar magit-tramp-hosts-alist nil
   "Alist of host -> directory")
 
@@ -49,15 +52,32 @@
     (let ((default-directory (magit-tramp-resolve-host target-host)))
       (=
        (magit-git-exit-code "cat-file" "-t"
-                            (format "%s:%s" target-user
-                                    target-localname))
+                            (magit-tramp-filerev target-user target-localname))
        0))))
 
-(defun magit-tramp-handle-expand-file-name
-    (filename &optional default-directory)
-  (if (or (not default-directory) (string-prefix-p "/" filename))
-      filename
-      (concat default-directory filename)))
+(defun magit-tramp-handle-expand-file-name (name &optional dir)
+  ;; If DIR is not given, use DEFAULT-DIRECTORY or "/".
+  (setq dir (or dir default-directory "/"))
+  ;; Unless NAME is absolute, concat DIR and NAME.
+  (unless (file-name-absolute-p name)
+    (setq name (concat (file-name-as-directory dir) name)))
+  ;; If NAME is not a Tramp file, run the real handler.
+  (if (not (tramp-connectable-p name))
+      (tramp-run-real-handler 'expand-file-name (list name nil))
+    ;; Dissect NAME.
+    (with-parsed-tramp-file-name name nil
+      (unless (tramp-run-real-handler 'file-name-absolute-p (list localname))
+	(setq localname (concat "~/" localname)))
+      (while (string-match "//" localname)
+	(setq localname (replace-match "/" t t localname)))
+      (let ((directory-sep-char ?/)
+	    (default-directory (tramp-compat-temporary-file-directory)))
+	(tramp-make-tramp-file-name
+	 method user host
+	 (tramp-drop-volume-letter
+	  (tramp-run-real-handler
+	   'expand-file-name (list localname)))
+	 hop)))))
 
 (defun magit-tramp-resolve-host (host)
   (let ((elem (assoc host magit-tramp-hosts-alist)))
@@ -78,16 +98,15 @@
    (with-parsed-tramp-file-name filename target
      (let ((default-directory (magit-tramp-resolve-host target-host)))
        (magit-git-string "cat-file" "-s"
-                         (format "%s:%s" target-user
-                                 target-localname))))))
+                         (magit-tramp-filerev target-user target-localname))))))
 
 (defun magit-tramp-handle-file-directory-p (filename)
   (with-parsed-tramp-file-name filename target
      (let ((default-directory (magit-tramp-resolve-host target-host)))
        (string= "tree"
                 (magit-git-string "cat-file" "-t"
-                                  (format "%s:%s" target-user
-                                          target-localname))))))
+                                  (magit-tramp-filerev target-user
+                                                       target-localname))))))
 
 (defun magit-tramp-handle-file-newer-than-file-p (file1 file2)
   ;; bare minimum to make this consistent
@@ -136,10 +155,12 @@
       (insert (substring
                (with-parsed-tramp-file-name filename target
                  (string-as-multibyte
-                  (let ((default-directory (magit-tramp-resolve-host target-host)))
-                    (magit-git-output (list "cat-file" "-p"
-                                            (format "%s:%s" target-user
-                                                    target-localname))))))
+                  (let ((default-directory (magit-tramp-resolve-host
+                                            target-host)))
+                    (magit-git-output
+                     (list "cat-file" "-p"
+                           (magit-tramp-filerev target-user
+                                                target-localname))))))
                (or beg 0) end))
       (list filename (magit-tramp-file-size filename))))
 
@@ -150,13 +171,114 @@
               (mapcar (lambda (name)
                         (and (string-prefix-p file name)
                              name))
-                      (magit-git-lines "ls-tree" "--name-only"
-                                       (format "%s:%s" target-user
-                                               target-localname)))))))
+                      (magit-git-lines
+                       "ls-tree" "--name-only"
+                       (magit-tramp-filerev target-user target-localname)))))))
+
+(defun magit-tramp-handle-directory-files-and-attributes
+    (directory &optional full match nosort id-format)
+  (mapcar (lambda (filename)
+            (cons filename (magit-tramp-handle-file-attributes
+                            (expand-file-name filename directory)
+                            id-format)))
+          (tramp-handle-directory-files directory)))
 
 (defun magit-tramp-handle-insert-directory
-    (filename switches &optional wildcard full-directory-p)
-  nil)
+  (filename switches &optional wildcard full-directory-p)
+  (setq filename (expand-file-name filename))
+  (if full-directory-p
+      ;; Called from `dired-add-entry'.
+      (setq filename (file-name-as-directory filename))
+    (setq filename (directory-file-name filename)))
+  (with-parsed-tramp-file-name filename nil
+    (save-match-data
+      (let ((base (file-name-nondirectory filename))
+	    (entries (magit-tramp-handle-directory-files-and-attributes
+                      (file-name-directory filename)
+                      nil nil nil 'string)))
+
+	(when wildcard
+	  ;; (string-match "\\." base)
+	  ;; (setq base (replace-match "\\\\." nil nil base))
+	  ;; (string-match "\\*" base)
+	  ;; (setq base (replace-match ".*" nil nil base))
+	  ;; (string-match "\\?" base)
+	  ;; (setq base (replace-match ".?" nil nil base))
+          )
+
+	;; Filter entries.
+	(setq entries
+	      (delq
+	       nil
+	       (if (or wildcard (zerop (length base)))
+		   ;; Check for matching entries.
+		   (mapcar
+		    (lambda (x)
+		      (when (string-match
+			     (format "^%s" base) (nth 0 x))
+			x))
+		    entries)
+		 ;; We just need the only and only entry FILENAME.
+		 (list (assoc base entries)))))
+
+	;; Sort entries.
+	(setq entries
+	      (sort
+	       entries
+	       (lambda (x y)
+		 (if (string-match "t" switches)
+		     ;; Sort by date.
+		     (tramp-time-less-p (nth 3 y) (nth 3 x))
+		   ;; Sort by name.
+		   (string-lessp (nth 0 x) (nth 0 y))))))
+
+	;; Handle "-F" switch.
+	(when (string-match "F" switches)
+	  (mapc
+	   (lambda (x)
+	     (when (not (zerop (length (car x))))
+	       (cond
+		((char-equal ?d (string-to-char (nth 1 x)))
+		 (setcar x (concat (car x) "/")))
+		((char-equal ?x (string-to-char (nth 1 x)))
+		 (setcar x (concat (car x) "*"))))))
+	   entries))
+
+	;; Print entries.
+	(mapc
+	 (lambda (x)
+	   (when (not (zerop (length (nth 0 x))))
+	     (let ((attr (ignore-errors
+                           (cdr x))))
+	       (insert
+		(format
+		 "%10s %3d %-8s %-8s %8s %s "
+		 (or (nth 8 attr)) ; mode
+		 (or (nth 1 attr)) ; inode
+		 (or (nth 2 attr)) ; uid
+		 (or (nth 3 attr)) ; gid
+		 (or (nth 7 attr)) ; size
+		 (format-time-string
+		  (if (tramp-time-less-p
+		       (tramp-time-subtract (current-time) (nth 5 attr))
+		       tramp-half-a-year)
+		      "%b %e %R"
+		    "%b %e  %Y")
+		  (nth 5 attr)))) ; date
+	       ;; We mark the file name.  The inserted name could be
+	       ;; from somewhere else, so we use the relative file
+	       ;; name of `default-directory'.
+	       (let ((start (point)))
+		 (insert
+		  (format
+		   "%s\n"
+		   (file-relative-name
+		    (expand-file-name
+		     (nth 0 x) (file-name-directory filename)))))
+		 (put-text-property start (1- (point)) 'dired-filename t))
+	       (forward-line)
+	       (beginning-of-line))))
+	 entries)))))
 
 (defconst magit-tramp-file-name-handler-alist
   '(
@@ -180,8 +302,8 @@
     (file-attributes . magit-tramp-handle-file-attributes)
     (file-modes . tramp-handle-file-modes)
     (directory-files . tramp-handle-directory-files)
-    ;; (directory-files-and-attributes
-    ;;  . tramp-sh-handle-directory-files-and-attributes)
+    (directory-files-and-attributes
+     . magit-tramp-handle-directory-files-and-attributes)
     (file-name-all-completions . magit-tramp-handle-file-name-all-completions)
     (file-name-completion . tramp-handle-file-name-completion)
     (add-name-to-file . ignore)
@@ -194,15 +316,15 @@
     (delete-directory . ignore)
     (delete-file . ignore)
     (directory-file-name . tramp-handle-directory-file-name)
-    ;; ;; `executable-find' is not official yet.
+    ;; `executable-find' is not official yet.
     (executable-find . ignore)
     (start-file-process . ignore)
     (process-file . ignore)
     (shell-command . ignore)
-    ;; (insert-directory . magit-tramp-handle-insert-directory)
+    (insert-directory . magit-tramp-handle-insert-directory)
     (expand-file-name . magit-tramp-handle-expand-file-name)
     (substitute-in-file-name . tramp-handle-substitute-in-file-name)
-    ;; (file-local-copy . tramp-sh-handle-file-local-copy)
+    (file-local-copy . ignore)
     (file-remote-p . tramp-handle-file-remote-p)
     (insert-file-contents . magit-tramp-handle-insert-file-contents)
     ;; TODO: do we need more logic here ?
